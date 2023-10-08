f649bba8914d63a66134ef3e6e983b4b87693371
Your Name <daniil.ibragimov.40@gmail.com>
2023-10-08 23:22:28 +0500

    add term 3 and first java lab

---
+++ term_3/java/Lr1Java/src/main/java/ru/ibragimov/Lr1Java/Lr1JavaApplication.java
@@ -0,0 +1,13 @@
+package ru.ibragimov.Lr1Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lr1JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lr1JavaApplication.class, args);
+	}
+
+}
---
+++ term_3/java/Lr1Java/src/test/java/ru/ibragimov/Lr1Java/Lr1JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ibragimov.Lr1Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lr1JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
